5374558090951da4869d2d5407968ab66422a8ae
Steven McManus <sm@seer.la>
2019-03-29 10:45:41 -0600

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+var dummyData = [];
+app.get('/api/get-data', function(req, resp) {
+  resp.json({data:dummyData});
+})
+app.post('/api/post-data', function(req, resp) {
+  var newData = req.body.data;
+  dummyData.push(newData);
+  resp.json({data:dummyData});
+});
+
+
+app.listen(9000, function() {
+  console.log("Server is up");
+});
