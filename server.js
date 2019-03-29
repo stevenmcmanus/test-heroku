b7908fd2e046b8bb03d409d8b429b3d13a58382e
Steven McManus <sm@seer.la>
2019-03-29 10:55:24 -0600

    initial test to staging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var dummyData = [];
+var dummyData = [{data:"staging"}];
 app.get('/api/get-data', function(req, resp) {
   resp.json({data:dummyData});
 })
